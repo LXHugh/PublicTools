bb479a6a906d27a0f9d73b8671a73244911a3536
刘宣 <13511029398@163.com>
2023-08-21 09:40:44 +0800

    代码上传

--- PublicToolsClass.h
+++ PublicToolsClass.h
@@ -11,6 +11,16 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface PublicToolsClass : NSObject
 
+#pragma mark -- view样式设置
+
+#pragma mark -- 获取系统信息
+
+#pragma mark -- 计算类方法
+
+#pragma mark -- 加密方法
+//MD5加密
++ (NSString *)md5Encryption:(NSString *)car;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- PublicToolsClass.m
+++ PublicToolsClass.m
@@ -7,6 +7,28 @@
 
 #import "PublicToolsClass.h"
 
+#import <CommonCrypto/CommonDigest.h>//MD5加密使用该库
+
 @implementation PublicToolsClass
+
+#pragma mark -- view样式设置
+
+#pragma mark -- 获取系统信息
+
+#pragma mark -- 计算类方法
+
+#pragma mark -- 加密方法
+//MD5加密
++ (NSString *)md5Encryption:(NSString *)car{
+    const char *input = [car UTF8String];
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(input, (CC_LONG)strlen(input), result);
+    NSMutableString *digest = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for (NSInteger i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [digest appendFormat:@"%02x", result[i]];
+    }
+    return digest;
+}
+
 
 @end
