90a63fe576f53caf15dbf8cd35895d6848a70d0f
刘宣 <13511029398@163.com>
2023-08-21 08:56:14 +0800

    添加工具类

---
+++ PublicToolsClass.h
@@ -0,0 +1,16 @@
+//
+//  PublicToolsClass.h
+//  PublicTools
+//
+//  Created by 刘宣 on 2023/8/19.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PublicToolsClass : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PublicToolsClass.m
@@ -0,0 +1,12 @@
+//
+//  PublicToolsClass.m
+//  PublicTools
+//
+//  Created by 刘宣 on 2023/8/19.
+//
+
+#import "PublicToolsClass.h"
+
+@implementation PublicToolsClass
+
+@end
